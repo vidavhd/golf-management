5d96680f83dd620761ef0cc09de3a5048ca8f655
Haradeep Vidavaluru <vidavaluruharadeep@gmail.com>
2025-10-30 22:44:34 -0500

    initial commit.

---
+++ src/main/java/com/management/golf_management/GolfManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.management.golf_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfManagementApiApplication.class, args);
+	}
+
+}
